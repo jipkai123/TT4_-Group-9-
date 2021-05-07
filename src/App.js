950c85b347ed6871ede1626cb3a930f23114526e
Thor Wei Jie <thorweijie@yahoo.com>
2021-05-07 12:34:29 +0800

    Import dependencies into App.js, removed center text alignment in App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import PrivateRoute from './Components/PrivateRoute';
+
 //import Components here
 
 function App() {
@@ -8,9 +12,6 @@ function App() {
 
 	routes = (
 		<Switch>
-			<PrivateRoute path="/home" component={ViewBalance} />
-			<PrivateRoute path="/AddTransaction" component={AddTransaction} />
-			<PrivateRoute path="/TransactionHistory" component={TransactionHistory} />
       <Route exact path='/login' component={Login} />
 			<Route exact path="/">
 				<Redirect to="/login" />
