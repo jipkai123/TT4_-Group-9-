c3c01237ed0ebc3ec6db9a9f87c16ee14c6e8122
jipkai123 <jipkai123@hotmail.com>
2021-05-07 15:45:05 +0800

    working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import 'bulma/css/bulma.css';
-//import './App.css';
-//import './styles.scss';
+import './App.css';
 import React, { Component, lazy, Suspense } from 'react';
 import PrivateRoute from './Components/PrivateRoute';
 import Login from './Components/Login';
 import { withRouter } from 'react-router';
 import NotFound  from './Components/NotFound';
-import NavBar from './Components/NavBar';
+//import NavBar from './Components/NavBar';
+import { Nav,NavDropdown,Form,FormControl,Button,Navbar } from 'react-bootstrap';
+
 import {
   BrowserRouter,
   Switch,
@@ -43,15 +42,31 @@ const LoginRoute = withRouter(
   lazy(() => import(/* webpackChunkName: "Login" */ './Components/Login'))
 );
 
+
+
 class App extends Component {
 
   render() {
     return (
       <BrowserRouter>
-        <NavBar />
+      
         <Switch>
           <Redirect from="/" exact to="/login" />
           <Route path="/login" component={Login} />
+          <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/login">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
           <PrivateRoute path="/home" component={ViewBalance} />
           <Route exact path="**" component={NotFound} />
         </Switch>
