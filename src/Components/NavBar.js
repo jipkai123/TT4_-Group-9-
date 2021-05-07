a5b1c6ce965af92daa9b206cdfeef24ece7752c7
jipkai123 <jipkai123@hotmail.com>
2021-05-07 15:15:09 +0800

    working

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Nav from "react-bootstrap/Nav";
 import { NavLink } from 'react-router-dom';
 
 const NavBar = props => (
   <nav className="column is-2 menu">
     <p className="menu-label">Menu</p>
-    <ul className="menu-list">
+    <ul className="menu-lbist">
       <NavLink to="/login" activeClassName="active-link">
         login
       </NavLink>
