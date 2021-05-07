86d93889487def758a7e355421f4d32d90e0dc7e
Thor Wei Jie <thorweijie@yahoo.com>
2021-05-07 13:09:57 +0800

    Initialise ViewBalance

---
+++ src/Components/ViewBalance.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Table} from 'react-bootstrap';
+import axios from 'axios';
+
+class ViewBalance extends Component {
+    state = {
+        accounts = []
+    };
+
+}
