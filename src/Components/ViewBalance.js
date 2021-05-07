9ceed693234bb058fa63f59ad0a3cff4e4dd180d
Thor Wei Jie <thorweijie@yahoo.com>
2021-05-07 13:28:53 +0800

    Add storing accountKey to login page and API for ViewBalance

--- src/Components/ViewBalance.js
+++ src/Components/ViewBalance.js
@@ -7,4 +7,30 @@ class ViewBalance extends Component {
         accounts = []
     };
 
+    componentDidMount() {
+        this.checkAmountBalance();
+      }
+    
+    checkAmountBalance () => {
+        const data = JSON.stringify({
+            custID: sessionStorage.getItem('custID'),
+            accountKey: sessionStorage.getItem('accountKey')
+        });
+     axios.post('https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/login',
+            {
+                headers: {
+                    "x-api-key": 'Jkx76CEYnp3NaTpwSXceo4ONDFLJNZcA717hzo1m',
+                },
+                data: data
+            })
+            .then((res) => {
+                console.log(res)
+                //history.replace("/home");
+
+            }).catch((error) => {
+                alert("Unable to retrieve account balance")
+                console.log(error)
+            });
+
+    }
 }
